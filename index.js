9e20a8ad3db908ff94a84a221582b1909ce08ffd
Christian Fei <crifei93@gmail.com>
2020-03-20 12:40:50 +0100

    send tg message

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const { sendMessageFor } = require('simple-telegram-message')
 
-try {
-  const message = core.getInput('message')
-  console.log(`Sending message "${message}"`)
+main()
+  .then(() => process.exit(0))
+  .catch((err) => console.error(err) && process.exit(1))
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const serializedPayload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${serializedPayload}`)
-} catch (error) {
-  core.setFailed(error.message)
+async function main () {
+  try {
+    const message = core.getInput('message')
+    console.log(`Sending message "${message}"`)
+
+    sendMessageFor(process.env.TELEGRAM_TOKEN, process.env.TELEGRAM_CHAT_ID || process.env.TELEGRAM_CHANNEL)(message)
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const serializedPayload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${serializedPayload}`)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
